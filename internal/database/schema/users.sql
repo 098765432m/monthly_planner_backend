b3131f3b99983ec4a633251dbdef8ce0422370ca
098765432m <anphutran73@gmail.com>
2025-05-10 14:13:53 +0700

    feat(repository): Integrate with sqlc.

---
+++ internal/database/schema/users.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(255) NOT NULL,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+)
